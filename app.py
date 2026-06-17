ef954517f9d5e4a4f2501bae390dcdb3b9a965a9
Zaker-Raza <zakerr60@gmail.com>
2026-06-17 22:21:06 +0530

    changes

--- app.py
+++ app.py
@@ -162,6 +162,9 @@ def logout():
     session['name']=None
     return render_template('adminlogin.html')
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
-app.run(debug=True)
